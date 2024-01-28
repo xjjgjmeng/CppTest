3970b2b0a4106f14810a84bae2125d4ff6360d8c
xjjgjmeng <xjjgjmeng@163.com>
2024-01-28 18:42:53 +0800

    创建cmake结构

---
+++ src/std_cstdlib_rand.cpp
@@ -0,0 +1,28 @@
+﻿#include <random>
+#include <iostream>
+
+// RAND_MAX：std::rand返回的最大值
+
+int main()
+{
+	// use current time as seed for random generator
+	std::srand(std::time(nullptr));
+	int random_variable = std::rand();
+	std::cout << "Random value on [0 " << RAND_MAX << "]: " << random_variable << '\n';
+
+	// 给std::srand的种子相同，std::rand产生的随机数也相同（pseudo-random）
+	for (auto i = 0; i < 3; ++i)
+	{
+		std::srand(666);
+		std::cout << "666: " << std::rand() << '\n';
+	}
+
+	/*
+	要取得[a,b)的随机整数，使用`(rand() % (b-a))+ a` （结果值含a不含b）。
+	要取得[a,b]的随机整数，使用`(rand() % (b-a+1))+ a` （结果值含a和b）。
+	要取得(a,b]的随机整数，使用`(rand() % (b-a))+ a + 1` （结果值不含a含b）。
+	要取得0～1之间的浮点数，可以使用`rand() / double(RAND_MAX)`。
+	*/
+
+	return 0;
+}
---
+++ src/std_random.cpp
@@ -0,0 +1,13 @@
+﻿#include <random>
+#include <iostream>
+
+int main()
+{
+	// 生成一个[0, 0xffff]之间的随机数
+	std::random_device rd;
+	std::uniform_int_distribution<int> dist(0, 0xFFFF);
+	auto i = dist(rd);
+	std::cout << i << std::endl;
+
+	return 0;
+}
